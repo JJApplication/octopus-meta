45fa6e47579ec50fc98ffa4f0fa3bea1c3490e36
landers <liaorenj@gmail.com>
2022-08-01 01:42:18 +0800

    feat：新增模型属性

--- meta_consts.go
+++ meta_consts.go
@@ -19,13 +19,14 @@ const (
 	TypeWebFront   = "FrontEnd"   // 前端
 	TypeMiddleWare = "MiddleWare" // 中间件
 	TypeDataStore  = "DataStore"  // 数据层
+	TypeModule     = "Module"     // 模块
 )
 
 // app的发布状态
 const (
-	Published = "published" // 已发布
-	Testing   = "testing"   // 测试中
-	Pending   = "pending"   // 待发布
+	Published  = "published"  // 已发布
+	Testing    = "testing"    // 测试中
+	Unreleased = "unreleased" // 待发布
 )
 
 // 配置文件类型
